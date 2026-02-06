e8d5998c7976d71c488a78bc7acc03da1bf42747
Yanis Chuhib-Forster <yanis.chouhib-forster@edu.univ-fcomte.fr>
2026-02-06 10:30:22 +0100

    page offre.id

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -16,3 +16,14 @@ export async function getOffres() {
 export async function getImageUrl(record, recordImage) {
     return db.files.getURL(record, recordImage);
 }
+
+//backend.js
+export async function getOffre(id) {
+    try {
+        const data = await db.collection('maison').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
+    }
+}
